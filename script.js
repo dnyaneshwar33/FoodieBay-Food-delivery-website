250bccfc5527b4ce50186a546e211ba81e51b142
Dnyaneshwar Vighne <125205721+dnyaneshwar33@users.noreply.github.com>
2023-09-27 13:51:00 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,32 @@
+ let navbar = document.querySelector('.header .navbar');
+
+ document.querySelector('#menu-btn').onclick = () => {
+     navbar.classList.toggle('active');
+     searchForm.classList.remove('active');
+     cart.classList.remove('active');
+     loginForm.classList.remove('active');
+ }
+
+ window.onscroll = () => {
+     navbar.classList.remove('active');
+ }
+
+ document.querySelector('.home').onmousemove = (e) => {
+
+     let x = (window.innerWidth - e.pageX * 2) / 90;
+     let y = (window.innerHeight - e.pageY * 2) / 90;
+
+     document.querySelector('.home .home-parallax-img').style.transform = `translateX(${y}px) translateY(${x}px)`;
+ }
+
+ document.querySelector('.home').onmouseleave = () => {
+
+     document.querySelector('.home .home-parallax-img').style.transform = `translateX(0px) translateY(0px)`;
+ }
+ const buttons = document.querySelectorAll('.navbar a');
+buttons.forEach(button => {
+  button.addEventListener('click', () => {
+    buttons.forEach(btn => btn.classList.remove('active'));
+    button.classList.add('active');
+  });
+});
